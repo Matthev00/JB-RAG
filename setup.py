a671c97747ae333bf227a98ee4fb83697a7943b0
Matthev00 <mateuszostaszewskififa@gmail.com>
2025-03-18 13:37:24 +0100

    Update dependencies in pyproject.toml and remove setup.cfg and setup.py files

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='JB-RAG',
-    version='0.1',
-    packages=find_packages(),
-    description='Retrieval-Augmented Generation (RAG) system over a code repository for a question-answering task.',
-    author='Mateusz Ostaszewski',
-)
