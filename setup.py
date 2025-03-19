40636976c5070b97b3ca1e040bd3a9560315fc72
Matthev00 <mateuszostaszewskififa@gmail.com>
2025-03-19 17:14:23 +0100

    feat: Update pyproject.toml for build system and add new dependencies; create setup.py for package management

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='JB-RAG',
+    version='0.1',
+    packages=find_packages(),
+    description='Code RAG',
+    author='Mateusz Ostaszewski',
+)
