085b1b2f1572cb9ac542d1ff945fb6b262cc7372
Matthev00 <mateuszostaszewskififa@gmail.com>
2025-03-19 19:39:13 +0100

    feat: Integrate FAISS retriever into main application logic and configure flake8 line length

--- src/main.py
+++ src/main.py
@@ -1,5 +1,16 @@
+from src.retriever.faiss_search import FAISSRetriever
+from src.config import EMBEDDING_MODEL
+
+
 def main():
-    print("Hello from jb-rag!")
+    retriever = FAISSRetriever(embedding_model=EMBEDDING_MODEL)
+    retriever.load_index("escrcpy")
+    results = retriever.search(
+        "How does the application determine and validate the paths for external binaries (like scrcpy, adb, and gnirehtet) at startup, and which module or file manages this logic?",
+        radius=0.3,
+    )
+    for result in results:
+        print(result["relative_path"])
 
 
 if __name__ == "__main__":
