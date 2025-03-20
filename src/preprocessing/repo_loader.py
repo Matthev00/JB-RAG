fc6b0a64866d7f1bfdd6916de9b643ce29394496
Matthev00 <mateuszostaszewskififa@gmail.com>
2025-03-20 10:46:27 +0100

    feat: Add docstrings to RepoLoader class and its methods for better documentation

--- src/preprocessing/repo_loader.py
+++ src/preprocessing/repo_loader.py
@@ -7,12 +7,25 @@ from src.config import REPO_DIR
 
 class RepoLoader:
     def __init__(self, repo_url: str):
+        """ 
+        Class for downloding repo from external source.
+        
+        Args:
+            repo_url (str): URL of the repository.
+        """
         self.repo_url = repo_url
         self.repo_dir = Path(REPO_DIR) / Path(
             repo_url.split("/")[-1].replace(".git", "")
         )
 
     def clone(self) -> Repo:
+        """
+        Clones the repository from the URL to the local directory.
+        Path is specified in config.py file as REPO_DIR.
+
+        Returns:
+            Repo: GitPython Repo object.
+        """
         if self.repo_dir.exists():
             return Repo(self.repo_dir)
         else:
